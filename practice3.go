cb111cc8c7513371010031c3e655703c7384b255
minimium <umekichi3hoshi@gmail.com>
2025-12-27 20:09:49 +0900

    Create practice3.go

---
+++ practice3.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"os"
+)
+
+func main() {
+	number := rand.IntN(100)
+	for i := 1; i <= 5; i++ {
+		var input int
+		fmt.Print("your prediction : ")
+		fmt.Scan(&input)
+
+		if input > number {
+			fmt.Println("BIG!")
+		} else if input < number {
+			fmt.Println("SMALL!!")
+		} else if input == number {
+			fmt.Println("CLEAR!!")
+			os.Exit(0)
+		} else {
+			fmt.Println("!?!?!?")
+		}
+	}
+	fmt.Println("game over")
+	fmt.Println("answer : ", number)
+}
