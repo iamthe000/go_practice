287eb3eeae6e6b29432034d20638d8774175502d
minimium <umekichi3hoshi@gmail.com>
2026-03-06 14:36:21 +0900

    Update practice18.go

--- practice18.go
+++ practice18.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
-    // \x1b[31m が赤色、\x1b[0m がリセット
-    fmt.Println("\x1b[31mこれは赤色の文字です\x1b[0m")
-    fmt.Println("\x1b[32mこれは緑色の文字です\x1b[0m")
-    fmt.Println("\x1b[34mこれは青色の文字です\x1b[0m")
+    var color string
+    for {
+        fmt.Print("color(red or green or blue) : ")
+        fmt.Scan(&color)
+        if color == "red" {
+            fmt.Println("\x1b[31mこれは赤色の文字です\x1b[0m")
+        } else if color == "green" {
+            fmt.Println("\x1b[32mこれは緑色の文字です\x1b[0m")
+        } else if color == "blue" {
+            fmt.Println("\x1b[34mこれは青色の文字です\x1b[0m")
+        } else if color == "end" {
+            fmt.Println("bye")
+            break
+        } else {
+            fmt.Println("??????????????????????????")
+        }
+    }
 }
