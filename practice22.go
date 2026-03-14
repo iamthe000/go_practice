1db5572260db97252ed078f0b8dce9f1dd474bbf
minimium <umekichi3hoshi@gmail.com>
2026-03-14 10:38:45 +0900

    Update practice22.go

--- practice22.go
+++ practice22.go
@@ -1,1 +1,11 @@
-strings.Replace("hello", "l", "L", 1)
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    test := strings.Replace("hello", "l", "L", 1)
+    fmt.Println(test)
+}
