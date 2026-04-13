f1abd2eda3755f452c89c44460040c6761691e4d
minimium <umekichi3hoshi@gmail.com>
2026-04-14 08:58:23 +0900

    practice27.go を更新

--- practice27.go
+++ practice27.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-    x := 16
-    result := math.Sqrt(float64(x)) 
+	x := 16
+	result := math.Sqrt(float64(x))//float64という小数点の使える数字の形式に変換
+	fmt.Println(result)
 }
